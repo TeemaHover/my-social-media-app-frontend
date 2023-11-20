6750ae2fb93298f545e884e2c5aab551980f7fbb
Teema <Teemahover@gmail.com>
2023-11-20 21:38:00 +0800

    asd

---
+++ src/Login.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('/api/login', { username, password });
+      console.log(response.data.message);
+    } catch (error) {
+      console.error(error.response.data.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/Register.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post('/api/register', { username, password });
+      console.log(response.data.message);
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
+};
+
+export default Register;
